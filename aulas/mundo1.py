e6411e662af8acd084af6005f2f5f24541cc1395
Joao Gabriel Borges Santos <joao.santos@RPA.REDE>
2025-03-18 08:40:26 -0300

    First Commit

---
+++ aulas/mundo1.py
@@ -0,0 +1,7 @@
+# import mundo3 as md
+# from models import dataValid
+# from menu_gguanabara import *
+import random as rd
+
+
+print()
---
+++ aulas/testing.py
@@ -0,0 +1,12 @@
+# # Skipping to ep22
+# interactive help
+
+def whaalie(a, palin=True):
+    if palin == None:
+        print('Noice')
+    elif palin:
+        print(a)
+    elif not palin:
+        print('ok so far')
+
+whaalie('ok', palin=None)
